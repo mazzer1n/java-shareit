48841627bb4694e1560d764d90e9db80333b2c75
Maxim Malyarov <malerovmplay@gmail.com>
2024-01-10 14:47:52 +0500

    fix

--- src/test/java/ru/practicum/shareit/booking/repository/BookingRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/booking/repository/BookingRepositoryTest.java
@@ -1,9 +1,10 @@
 package ru.practicum.shareit.booking.repository;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.*;
-import org.springframework.data.domain.*;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
@@ -12,8 +13,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static ru.practicum.shareit.booking.model.Status.*;
-import static ru.practicum.shareit.booking.service.BookingServiceImpl.SORT;
+import static ru.practicum.shareit.booking.model.Status.APPROVED;
 
 @DataJpaTest
 public class BookingRepositoryTest {
