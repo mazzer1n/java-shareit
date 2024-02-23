fe160fe8372cfcf4c67c93400d84b31869d687ee
Maxim Malyarov <malerovmplay@gmail.com>
2024-02-24 02:57:47 +0500

    add-docker

--- server/src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ server/src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -3,12 +3,10 @@ package ru.practicum.shareit.item.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.practicum.shareit.item.dto.*;
+import ru.practicum.shareit.item.dto.CommentDto;
+import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.service.ItemService;
 
-import javax.validation.Valid;
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.PositiveOrZero;
 import java.util.ArrayList;
 import java.util.Collection;
 
