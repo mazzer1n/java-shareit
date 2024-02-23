547506126bb96d3f416739ebbcf5c00e61887589
Maxim Malyarov <malerovmplay@gmail.com>
2024-02-24 03:27:38 +0500

    add-docker

--- server/src/test/java/ru/practicum/shareit/request/service/ItemRequestServiceTest.java
+++ server/src/test/java/ru/practicum/shareit/request/service/ItemRequestServiceTest.java
@@ -11,7 +11,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import ru.practicum.shareit.core.exception.exceptions.PaginationBadRequestException;
 import ru.practicum.shareit.core.exception.exceptions.RequestNotFoundException;
 import ru.practicum.shareit.item.service.ItemServiceImpl;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
